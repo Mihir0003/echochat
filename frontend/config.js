dca6177d4bd2df55646cfe4938830da7e0bb4324
Mihir.Amodwala <Mihir.amodwala@zmail.com>
2026-06-05 14:54:58 +0530

    Update backend URL

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 // Overwritten on Vercel deploy. Used when frontend and backend are on different hosts.
 window.ECHOCHAT_CONFIG = window.ECHOCHAT_CONFIG || {
-  backendOrigin: 'https://echochat-hq.onrender.com'
+  backendOrigin: 'https://echochat-hrnl.onrender.com'
 };
